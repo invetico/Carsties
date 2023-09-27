82ac295c6e6773b128f0326e4c223a86b0859db2
Sajith Jayaratne <sj@invetico.com.au>
2023-09-27 10:22:41 +1000

    End of section 2

---
+++ src/AuctionService/Data/Migrations/20230925133727_dbCorrection.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class dbCorrection : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ApplicationModels",
+                table: "Items",
+                newName: "Models");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Models",
+                table: "Items",
+                newName: "ApplicationModels");
+        }
+    }
+}
---
+++ src/AuctionService/Data/Migrations/20230925133855_dbCorrection-2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class dbCorrection2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Models",
+                table: "Items",
+                newName: "Model");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Model",
+                table: "Items",
+                newName: "Models");
+        }
+    }
+}
